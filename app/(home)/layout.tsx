927285f11d3e81559c0648fc2cdd7a4f1584f3e6
ChiSamuel651 <chijoe0306@gmail.com>
2024-12-11 00:52:12 +0100

    header configuration

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,22 +1,26 @@
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { redirect } from 'next/navigation';
 import React from 'react'
+import Header from './_components/common/Header';
 
 const MainLayout = async ({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) => {
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
 
-  const {isAuthenticated} = getKindeServerSession();
+  const { isAuthenticated } = getKindeServerSession();
   const isUserAuthenticated = await isAuthenticated();
 
-  if(!isUserAuthenticated){
+  if (!isUserAuthenticated) {
     redirect('/')
   }
   return (
-    <div>
-      {children}
+    <div className='w-full h-auto min-h-screen !bg-[#f8f8f8] dark:!bg-background'>
+      <Header />
+      <div>
+        {children}
+      </div>
     </div>
   )
 }
