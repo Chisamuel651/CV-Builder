6c45c9545574547fd3dd119ad788d3377f7fe49a
ChiSamuel651 <chijoe0306@gmail.com>
2024-12-10 13:19:42 +0100

    kinde auth configuration

--- app/(LandingPage)/layout.tsx
+++ app/(LandingPage)/layout.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from 'next/navigation';
 
-const LandingLayout = ({
+const LandingLayout = async ({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) => {
+  const {isAuthenticated} = getKindeServerSession();
+  const isUserAuthenticated = await isAuthenticated();
+
+  if(isUserAuthenticated){
+    redirect('/dashboard')
+  }
+
+  console.log(isUserAuthenticated, 'isUserAuthenticated');
+
   return (
     <div>
       {children}
--- app/(LandingPage)/page.tsx
+++ app/(LandingPage)/page.tsx
@@ -1,6 +1,10 @@
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Home() {
   return (
-    <div>landing page</div>
+    <div>
+      <LoginLink>Sign in</LoginLink>
+      <RegisterLink>Sign up</RegisterLink>
+    </div>
   );
 }
