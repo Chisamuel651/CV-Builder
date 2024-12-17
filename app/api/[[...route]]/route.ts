2a17e1f6a261fea86ff0f29c92e613aeb1671f9d
ChiSamuel651 <chijoe0306@gmail.com>
2024-12-17 09:39:02 +0100

    protected api routes for HonoJS V1.0

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,13 +2,14 @@ import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import { z } from 'zod'
 import { zValidator } from '@hono/zod-validator'
+import { getAuthUser } from '@/lib/kinde'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
 app
-    .get('/hello', (c) => {
+    .get('/hello', getAuthUser, (c) => {
         return c.json({
             message: 'Hello Next.js!',
         })
