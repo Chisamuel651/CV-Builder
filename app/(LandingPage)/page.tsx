f9529e668d81e25803ebfc8d91abe5101258966a
ChiSamuel651 <chijoe0306@gmail.com>
2025-01-02 23:45:33 +0100

    database setup with vercel postgres V6.0

--- app/(LandingPage)/page.tsx
+++ app/(LandingPage)/page.tsx
@@ -3,8 +3,8 @@ import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
 export default function Home() {
   return (
     <div>
-      <LoginLink>Sign in</LoginLink>
-      <RegisterLink>Sign up</RegisterLink>
+      <LoginLink>Sign-in</LoginLink>
+      <RegisterLink>Sign-up</RegisterLink>
     </div>
   );
 }
