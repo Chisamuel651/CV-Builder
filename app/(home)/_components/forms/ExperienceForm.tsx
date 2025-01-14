6bcd548e56c43a630fad7a3b9bd0a70d764d90dd
ChiSamuel651 <chijoe0306@gmail.com>
2025-01-14 13:46:02 +0100

    Work experience Form Section V2

--- app/(home)/_components/forms/ExperienceForm.tsx
+++ app/(home)/_components/forms/ExperienceForm.tsx
@@ -1,10 +1,13 @@
+import RichTextEditor from '@/components/editor';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { useResumeContext } from '@/context/resume-info-provider';
 import useUpdateDocument from '@/features/document/user-update-document';
+import { toast } from '@/hooks/use-toast';
+import { generateThumbnail } from '@/lib/helper';
 import { Loader, Plus, X } from 'lucide-react';
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
 const initialState = {
     id: undefined,
@@ -18,25 +21,25 @@ const initialState = {
     currentlyWorking: false,
 }
 
-const ExperienceForm = ( props: {handleNext:() => void} ) => {
-    const {handleNext} = props;
-    const {resumeInfo, onUpdate} = useResumeContext();
+const ExperienceForm = (props: { handleNext: () => void }) => {
+    const { handleNext } = props;
+    const { resumeInfo, onUpdate } = useResumeContext();
     const { mutateAsync, isPending } = useUpdateDocument()
 
-    const [ experienceList, setExperienceList ] = useState(() => {
+    const [experienceList, setExperienceList] = useState(() => {
         return resumeInfo?.experiences?.length ? resumeInfo.experiences : [initialState];
     });
 
     useEffect(() => {
-        if(!resumeInfo) return;
+        if (!resumeInfo) return;
         onUpdate({
             ...resumeInfo,
             experiences: experienceList
         })
     }, [experienceList])
 
-    const handleChange = (e:{target: {name: string, value: string}}, index:number) => {
-        const {name, value} = e.target;
+    const handleChange = (e: { target: { name: string, value: string } }, index: number) => {
+        const { name, value } = e.target;
         setExperienceList((prevState) => {
             const newExperienceList = [...prevState]
             newExperienceList[index] = {
@@ -57,87 +60,132 @@ const ExperienceForm = ( props: {handleNext:() => void} ) => {
         setExperienceList(updatedExperience)
     }
 
-  return (
-    <div>
-      <div className="w-full capitalize">
-        <h2 className="font-bold text-lg">professional experience</h2>
-        <p className="text-sm">add previous job experience</p>
-      </div>
+    const handleEditor = (value: string, name: string, index: number) => {
+        setExperienceList((prevState) => {
+            const newExperienceList = [...prevState]
+            newExperienceList[index] = {
+                ...newExperienceList[index],
+                [name]: value,
+            };
+            return newExperienceList;
+        })
+    }
 
-      <form>
-        <div className="border w-full h-auto divide-y-[1px] rounded-md px-3 pb-4 my-5">
-            {experienceList?.map((item, index) => (
-                <div key={index}>
-                    <div className='relative grid grid-cols-2 mb-5 pt-4 gap-3'>
-                        {experienceList?.length > 1 && (
-                            <Button 
-                                variant='secondary' 
-                                type='button' 
-                                className='size-[20px] text-center rounded-full absolute -top-3 -right-5 !bg-black dark:!bg-gray-600 text-white'
-                                size='icon'
-                                onClick={() => removeNewExperience(index)}
-                            >
-                                <X size='13px' />
-                            </Button>
-                        )}
+    const handleSubmit = useCallback(async (e: { preventDefault: () => void }) => {
+        e.preventDefault();
+        const thumbnail = await generateThumbnail();
+        const currentNo = resumeInfo?.currentPosition ? resumeInfo.currentPosition + 1 : 1;
 
-                        <div>
-                            <Label className='text-sm'>position title</Label>
-                            <Input name='title' placeholder='' required value={item?.title || ''} onChange={(e) => handleChange(e, index)} />
+        await mutateAsync({
+            currentPosition: currentNo,
+            thumbnail: thumbnail,
+            experience: experienceList
+        }, {
+            onSuccess: () => {
+                toast({
+                    title: 'Success',
+                    description: 'Experience updated successfully'
+                });
+                handleNext()
+            },
+            onError() {
+                toast({
+                    title: 'Error',
+                    description: 'Failed to updatred experience',
+                    variant: 'destructive',
+                });
+            }
+        });
+    }, [resumeInfo, experienceList]);
+
+
+
+    return (
+        <div>
+            <div className="w-full capitalize">
+                <h2 className="font-bold text-lg">professional experience</h2>
+                <p className="text-sm">add previous job experience</p>
+            </div>
+
+            <form onSubmit={handleSubmit}>
+                <div className="border w-full h-auto divide-y-[1px] rounded-md px-3 pb-4 my-5">
+                    {experienceList?.map((item, index) => (
+                        <div key={index}>
+                            <div className='relative grid grid-cols-2 mb-5 pt-4 gap-3'>
+                                {experienceList?.length > 1 && (
+                                    <Button
+                                        variant='secondary'
+                                        type='button'
+                                        className='size-[20px] text-center rounded-full absolute -top-3 -right-5 !bg-black dark:!bg-gray-600 text-white'
+                                        size='icon'
+                                        onClick={() => removeNewExperience(index)}
+                                    >
+                                        <X size='13px' />
+                                    </Button>
+                                )}
+
+                                <div>
+                                    <Label className='text-sm'>position title</Label>
+                                    <Input name='title' placeholder='' required value={item?.title || ''} onChange={(e) => handleChange(e, index)} />
+                                </div>
+
+                                <div>
+                                    <Label className='text-sm'>company name</Label>
+                                    <Input name='companyName' placeholder='' required value={item?.companyName || ''} onChange={(e) => handleChange(e, index)} />
+                                </div>
+
+                                <div>
+                                    <Label className='text-sm'>city</Label>
+                                    <Input name='city' placeholder='' required value={item?.city || ''} onChange={(e) => handleChange(e, index)} />
+                                </div>
+
+                                <div>
+                                    <Label className='text-sm'>state or region</Label>
+                                    <Input name='state' placeholder='' required value={item?.state || ''} onChange={(e) => handleChange(e, index)} />
+                                </div>
+
+                                <div>
+                                    <Label className='text-sm'>start date</Label>
+                                    <Input name='startDate' placeholder='' type='date' required value={item?.startDate || ''} onChange={(e) => handleChange(e, index)} />
+                                </div>
+
+                                <div>
+                                    <Label className='text-sm'>end date</Label>
+                                    <Input type='date' name='endDate' placeholder='' required value={item?.endDate || ''} onChange={(e) => handleChange(e, index)} />
+                                </div>
+
+                                <div className="mt-1 col-span-2">
+                                    {/* {WorkSummary} */}
+                                    <RichTextEditor
+                                        jobTitle={item.title || ""}
+                                        initialValue={item.workSummary || ""}
+                                        onEditorChange={(value: string) => handleEditor(value, "workSummary", index)}
+                                    />
+                                </div>
+                            </div>
+
+                            {index === experienceList.length - 1 && experienceList.length < 5 && (
+                                <Button
+                                    className='gap-1 mt-1 text-primary border-primary/50'
+                                    variant='outline'
+                                    type='button'
+                                    onClick={addNewExperience}
+                                >
+                                    <Plus size='15px' />
+                                    Add More Experience
+                                </Button>
+                            )}
                         </div>
-
-                        <div>
-                            <Label className='text-sm'>company name</Label>
-                            <Input name='companyName' placeholder='' required value={item?.companyName || ''} onChange={(e) => handleChange(e, index)} />
-                        </div>
-
-                        <div>
-                            <Label className='text-sm'>city</Label>
-                            <Input name='city' placeholder='' required value={item?.city || ''} onChange={(e) => handleChange(e, index)} />
-                        </div>
-
-                        <div>
-                            <Label className='text-sm'>state or region</Label>
-                            <Input name='state' placeholder='' required value={item?.state || ''} onChange={(e) => handleChange(e, index)} />
-                        </div>
-
-                        <div>
-                            <Label className='text-sm'>start date</Label>
-                            <Input name='startDate' placeholder='' type='date' required value={item?.startDate || ''} onChange={(e) => handleChange(e, index)} />
-                        </div>
-
-                        <div>
-                            <Label className='text-sm'>end date</Label>
-                            <Input name='endDate' placeholder='' required value={item?.endDate || ''} onChange={(e) => handleChange(e, index)} />
-                        </div>
-
-                        <div className="mt-1 col-span-2">
-                            {/* {WorkSummary} */}
-                        </div>
-
-                        {index === experienceList.length - 1 && experienceList.length < 5 && (
-                            <Button 
-                                className='gap-1 mt-1 text-primary border-primary/50' 
-                                variant='outline' 
-                                type='button' 
-                                onClick={addNewExperience}
-                            >
-                                <Plus size='15px' />
-                                Add More Experience
-                            </Button>
-                        ) } 
-                    </div>
+                    ))}
                 </div>
-            ))}
-        </div>
 
-        <Button className='mt-4' type='submit' disabled={isPending}>
-            {isPending && <Loader size='15px' className='aniamte-spin' />}
-            save changes
-        </Button>
-      </form>
-    </div>
-  )
+                <Button className='mt-4' type='submit' disabled={isPending}>
+                    {isPending && <Loader size='15px' className='aniamte-spin' />}
+                    save changes
+                </Button>
+            </form>
+        </div>
+    )
 }
 
 export default ExperienceForm
---
+++ components/editor/index.tsx
@@ -0,0 +1,131 @@
+import React, { useEffect, useState } from 'react'
+import {
+    BtnBold,
+    BtnBulletList,
+    BtnItalic,
+    BtnLink,
+    BtnNumberedList,
+    BtnStrikeThrough,
+    BtnUnderline,
+    Editor,
+    EditorProvider,
+    Separator,
+    Toolbar
+} from 'react-simple-wysiwyg'
+import { Label } from '../ui/label';
+import { Button } from '../ui/button';
+import { Loader, Sparkles } from 'lucide-react';
+import { toast } from '@/hooks/use-toast';
+import { AIChatSession } from '@/lib/google-ai-model';
+
+const PROMPT = `Given the job title "{jobTitle}",
+ create 6-7 concise and personal bullet points in
+  HTML stringify format that highlight my key
+  skills, relevant technologies, and significant
+   contributions in that role. Do not include
+    the job title itself in the output. Provide
+     only the bullet points inside an unordered
+     list.`;
+
+const RichTextEditor = (props: {
+    jobTitle: string | null;
+    initialValue: string;
+    onEditorChange: (e: any) => void;
+}) => {
+    const { jobTitle, initialValue, onEditorChange } = props;
+    const [loading, setLoading] = useState(false);
+    const [value, setValue] = useState(initialValue || "")
+
+    useEffect(() => {
+        setValue(initialValue || "")
+    }, [initialValue])
+
+    const GenerateSummaryFromAI = async () => {
+        try {
+            if (!jobTitle) {
+                toast({
+                    title: 'You must provide a Job position',
+                    variant: 'destructive',
+                });
+                return;
+            }
+            setLoading(true);
+            const prompt = PROMPT.replace("{jobTitle}", jobTitle || "Job Title");
+            const result = await AIChatSession.sendMessage(prompt);
+            const responseText = await result.response.text();
+            try {
+                const validJsonArray = JSON.parse(responseText);
+                const combinedHTML = validJsonArray.join("").trim();
+    
+                setValue(combinedHTML);
+                onEditorChange(combinedHTML);
+            } catch (parseError) {
+                toast({
+                    title: "Error",
+                    description: "AI response could not be parsed.",
+                    variant: "destructive",
+                });
+            }
+        } catch (error) {
+            toast({
+                title: 'Failed to generate the summary',
+                variant: 'destructive',
+            })
+        } finally {
+            setLoading(false);
+        }
+    }
+    return (
+        <div>
+            <div className="flex capitalize items-center justify-between my-2">
+                <Label>work summary</Label>
+                <Button
+                    variant='outline'
+                    className='gap-1'
+                    type='button'
+                    disabled={loading}
+                    onClick={() => GenerateSummaryFromAI()}
+                >
+                    <>
+                        <Sparkles size='15px' className='text-orange-500' />
+                        Generate With AI
+                    </>
+                    {loading && <Loader size='13px' className='aniamte-spin' />}
+                </Button>
+            </div>
+
+            <EditorProvider>
+                <Editor
+                    value={value}
+                    containerProps={{
+                        style: {
+                            resize: 'vertical',
+                            lineHeight: 1.2,
+                            fontSize: '13.5px',
+                        }
+                    }}
+                    onChange={(e) => {
+                        setValue(e.target.value);
+                        onEditorChange(e.target.value);
+                    }}
+                >
+                    <Toolbar>
+                        <BtnBold />
+                        <BtnItalic />
+                        <BtnUnderline />
+                        <BtnStrikeThrough />
+                        <Separator />
+                        <BtnNumberedList />
+                        <BtnBulletList />
+                        <Separator />
+                        <BtnLink />
+                    </Toolbar>
+                </Editor>
+            </EditorProvider>
+
+            
+        </div>
+    )
+}
+
+export default RichTextEditor
