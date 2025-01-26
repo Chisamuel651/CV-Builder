26fc41ac8d977753dabd11419c07395c82ae5c95
ChiSamuel651 <chijoe0306@gmail.com>
2025-01-26 01:20:11 +0100

    Deployment Fixes

--- app/(home)/_components/ResumeList.tsx
+++ app/(home)/_components/ResumeList.tsx
@@ -9,7 +9,7 @@ const ResumeList = () => {
   const resumes = data?.data ?? [];
   return (
     <Fragment>
-      { isLoading ? (<div className='flex items-center justify-center mx-5'>
+      {isLoading ? (<div className='flex items-center justify-center mx-5'>
         <Loader className='animate-spin text-black dark:text-white size-10' />
       </div>) : isError ? (
         <div className='flex flex-col items-center justify-center mx-5'>
@@ -20,21 +20,29 @@ const ResumeList = () => {
             <span>retry</span>
           </button>
         </div>
-      ): (
+      ) : (
         <>
-           {resumes?.map((resume) => (
-            <ResumeItem 
-              key={resume.documentId}
-              documentId={resume.documentId}
-              title={resume.title}
-              status={resume.status}
-              updatedAt={resume.updatedAt}
-              themeColor={resume.themeColor}
-              thumbnail={resume.thumbnail}
-            />
-           ))} 
+          {Array.isArray(resumes) && resumes.length > 0 ? (
+            <div className="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 lg:grid-cols-3">
+              {resumes.map((resume) => (
+                <ResumeItem
+                  key={resume.documentId}
+                  documentId={resume.documentId}
+                  title={resume.title}
+                  status={resume.status}
+                  updatedAt={resume.updatedAt}
+                  themeColor={resume.themeColor}
+                  thumbnail={resume.thumbnail}
+                />
+              ))}
+            </div>
+          ) : (
+            <div className="col-span-full text-center text-gray-500">
+              No resumes available.
+            </div>
+          )}
         </>
-      ) }
+      )}
     </Fragment>
   )
 }
